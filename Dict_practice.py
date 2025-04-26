d4881e5232437fe309554ea194cfbbe35dd847fc
zsavery <zsavery998@gmail.com>
2025-04-26 15:19:07 -0500

    Add list examples for comparison

--- Dict_practice.py
+++ Dict_practice.py
@@ -2,8 +2,9 @@
 if __name__ == '__main__':
     # Creating and empty dictionary
     myProfile = {}
+    #my_profile_lst = []
     # Printing a dictionary
-    #print(myProfile)
+    print(myProfile)
 
     # adding values to a dictionary
     myProfile = {"name": "Carter",
@@ -11,14 +12,22 @@ if __name__ == '__main__':
                  "city": "Frisco",
                  "state": "Texas"}
 
+    #my_profile_lst = ["Carter", 26, "Frisco", "Texas"]
+
     # Printing a dictionary (full)
-    #print(myProfile)
+    print(myProfile)
+    # print(my_profile_arr)
     # Printing a dictionary (single value)
-    #print(myProfile["age"])
+    print(myProfile["age"])
+    # print(my_profile_arr[1])
 
     # Adding new key:value pair to a dictionary
+    myProfile["age"] = 20
+    # my_profile_lst[1] = 34
     myProfile["job"] = "Curriculum Developer"
+    # my_profile_lst.append("Nun")
     myProfile['hobbies'] = ['fishing', 'programming', 'hiking']
+    # my_profile_lst.append(['fishing', 'programming', 'hiking'])
 
     # Modifying existing key:value pairs
     myProfile["name"] = "Jason"

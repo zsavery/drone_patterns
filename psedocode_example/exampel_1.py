37050ffb0b6436bcdcedeb8a6d25fb4eb407a4e5
Zyon Savery <zsavery998@gmail.com>
2025-05-17 14:42:11 -0500

    docs: pseudocode

---
+++ psedocode_example/exampel_1.py
@@ -0,0 +1,12 @@
+"""
+Start
+    Make Bed
+    Brush Teeth
+     If Is Box Empty
+        Open new Box
+     Eat Breakfast
+     Go to School
+End
+https://lucid.app/lucidchart/711bb30d-84d9-44aa-ba01-f4eb9238e7fb/edit?view_items=0czgPcRe0Aod%2CTgzgPzbWiw3U%2CGgzgR_Jsy2RN%2CQezgEl2TFUuq%2CfhzgcfN8eQWT%2Cdnzg4gA5wVBm&invitationId=inv_615a9ae5-42fa-4a3f-83c8-3d5c63963416
+"""
+

89256d6f09b0f5f94be72dc926068b7e89c5f4c8
Zyon Savery <zsavery998@gmail.com>
2025-05-31 11:49:31 -0500

    feat: add recursive starter

---
+++ recursion/factorial.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    print(f"factorial({n})")
+
+if __name__ == '__main__':
+    # Get
+    num = None
+
+    # Call factorial
+    print("Factorial of number", num, "=", recursive_factorial(num))
---
+++ recursion/fib.py
@@ -0,0 +1,38 @@
+def recursive_fibonacci(n):
+    print(f"recursive_fibonacci({n})")
+
+def iterative_fibonacci(n):
+    print(f"iterative_fibonacci({n})")
+
+
+if __name__ == '__main__':
+
+    # Change this to be user input if you wish!
+    n_terms = 0
+
+    while True:
+        n_terms = int(input("Number of terms: "))
+        # check if the number of terms is valid
+        if n_terms > 0:
+            break
+        else:
+            print("Invalid input ! Please input a positive value")
+
+    fibonacci_version = "None"
+
+
+    while fibonacci_version == "None":
+        fibonacci_version = input(f"Fibonacci version\n1. Series\n2. Recursive\n3. Exit\nEnter your choice: ")
+        match fibonacci_version.lower():
+            case "series":
+                recursive_fibonacci(fibonacci_version)
+                break
+            case "recursive":
+                iterative_fibonacci(fibonacci_version)
+                break
+            case "exit"|"quit"|"q":
+                break
+            case _:
+                fibonacci_version = "None"
+
+
